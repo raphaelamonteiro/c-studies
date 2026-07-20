e98310e140f35121c24e70ef640acad4ee3cf8db
Raphaela <bmraphaela@gmail.com>
2026-07-20 15:35:26 -0300

    feat: starting with variables

---
+++ 01-basics/guessing-game/guessing.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // Header
+    printf("---------------------------\n");
+    printf("Let's play a guessing game!\n");
+    printf("---------------------------\n");
+
+    int secretNumber = 42;
+
+    printf("The secret number is %d. Shhhhhh \n", secretNumber);
+}
